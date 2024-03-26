3323ae3f99c9b7d11707750c7b511b34b31fa790
Essa-ea2105851 <157367635+Essa-ea2105851@users.noreply.github.com>
2024-03-27 02:26:01 +0300

    progress

---
+++ Project/JavaScript/Cart.js
@@ -0,0 +1,7 @@
+export default class Cart {
+    constructor(cart_id, user_id, itemsInCart){
+        this.cart_id = cart_id;
+        this.user_id = user_id;
+        this.itemsInCart = itemsInCart;
+    }
+}
--- Project/JavaScript/cart-Repo.js
+++ Project/JavaScript/cart-Repo.js
@@ -1,4 +1,4 @@
-import Item from "./Item";
+import Item from "./Item.js";
 
 window.onload = async () => {
     await showCart();
@@ -6,18 +6,29 @@ window.onload = async () => {
   
   const BASE_URL = "";
   
-    let cartArray = [];
-  
+    let cartsArray = [];
+
     window.onload = async () => {
       await showCart();
       attachEventListeners();
     };
 
 function attachEventListeners() {
-      
+      const submitBtns  = document.querySelectorAll(".submit-Btn")
 };
 
 async function showCart(){
-    const storedCart = localStorage.getItem("cartArray");
-    
+    const storedCart = localStorage.getItem("cartsArray");
+    cartsArray = JSON.parse(storedCart)
+    if (!storedCart || cartsArray.length==0){
+        const emptyCart = document.createElement("div");
+        emptyCart.classList.add("empty");
+        emptyCart.innerHTML = "<h2>Cart is empty</h2>";
+        const cartDiv = document.getElementById("ifCartEmpty");
+        cartDiv.appendChild(emptyCart);
+    }
+
+    else{
+        cartsArray.map()
+    }
 }

4d5baa97a6280a91191834bae0426c8b26cd64a3
Essa-ea2105851 <157367635+Essa-ea2105851@users.noreply.github.com>
2024-03-28 04:11:47 +0300

    progress

--- Project/JavaScript/cart-Repo.js
+++ Project/JavaScript/cart-Repo.js
@@ -8,27 +8,29 @@ window.onload = async () => {
     await showCart();
   };
   
-  const BASE_URL = "";
+const BASE_URL = "";
   
-    let cartsArray = [];
+let cartsArray = [];
 
-    window.onload = async () => {
-      await showCart();
-      attachEventListeners();
-    };
+window.onload = async () => {
+  await showCart();
+  attachEventListeners();
+};
 
 function attachEventListeners() {
       const submitBtns  = document.querySelectorAll(".submit-Btn")
 };
 
+const item1 = new Item(1, "../images/product-1.jpg", "Red Shirt", 3.00, 5, 2);
+const item2 = new Item(2, "../images/product-2.jpg", "Black Running Shoes", 52.00, 10, 1);
+const item3 = new Item(3, "../images/product-3.jpg", "Buttoned joggers", 35.00, 20, 1);
+        
+cartsArray.push(new Cart(1, 3, [item1, item2, item3]));
+
 async function showCart(){
     // const storedCart = localStorage.getItem("cartsArray");
     // cartsArray = JSON.parse(storedCart);
-    const item1 = new Item(1, "../images/product-1.jpg", "Red Shirt", 3.00, 5, 2);
-    const item2 = new Item(2, "../images/product-2.jpg", "Black Running Shoes", 52.00, 10, 1);
-    const item3 = new Item(1, "../images/product-3.jpg", "Buttoned joggers", 35.00, 20, 1);
-        
-    cartsArray.push(new Cart(1, 3, [item1, item2, item3]));
+    
 
     if (!cartsArray){
         const emptyCart = document.createElement("div");
@@ -43,11 +45,36 @@ async function showCart(){
         
         const cartNeededArr = cartsArray.filter(cart => cart.user_id!=userId);
         const cart = cartsArray[0];
-        console.log(cart.itemsInCart);
+        showTotalPrice();
         cart.itemsInCart.forEach(element => {
             createItemCard(element)
         });
     }
+
+    function showTotalPrice(){
+        const cart = cartsArray[0];
+        const itemsinsidecart = cart.itemsInCart;
+        const totalPriceEle = document.createElement("p");
+        totalPriceEle.setAttribute('id', "totalPrice");
+        let totalPrice = 0;
+        
+        itemsinsidecart.forEach(item => totalPrice+=(item.item_price*item.quantitySelected));
+
+        totalPriceEle.textContent = `Total Price:   ${totalPrice}`; 
+        cartDiv.appendChild(totalPriceEle);
+    }
+}
+
+
+function removeFromCart(parentCard){
+    const itemId = parentCard.dataset.id;
+    const cart = cartsArray[0];
+    const cartItems = cart.itemsInCart;
+    const itemToRemove = cartItems.find(item => item.item_id == itemId);
+    parentCard.remove();
+    cartItems.splice(cartItems.indexOf(itemToRemove), 1);
+    console.log(cartItems);
+    //localStorage.setItem("cartsArray", cartsArray);
 }
 
 function updateQuantity(item){
@@ -58,19 +85,21 @@ function updateQuantity(item){
 function createItemCard(item){
     const parentDiv = document.getElementById("cartDiv")
     const itemCard = document.createElement("div");
+    itemCard.dataset.id = item.item_id;
     itemCard.classList.add("item-card");
-
+    const itemId = item.item_id; 
     itemCard.innerHTML = `
         <img src=${item.item_img} alt="itemImage">
         <h3>${item.item_name}</h3>
+        <p hidden>${itemId}</p>
         <br>
         <label for="quantity">Quantity:</label>
-        <input type="number" data-id="${item.item_id}" id="quantity" name="quantity" min="1" max="${item.item_stock}">
+        <input type="number" data-id="${itemId}" id="quantity" name="quantity" min="1" max="${item.item_stock}">
         <input onclick="updateQuantity(${item})" type="submit">
         <br>
         <p>Price:  $${item.item_price*item.quantitySelected}</p>
         <br>
-        <button onclick="removeFromCart()" class="deleteItem">Delete</button>
+        <button class="deleteItem">Delete</button>
     `;
     parentDiv.appendChild(itemCard);
     const quant = document.querySelector(`input[type='number'][data-id='${item.item_id}']`)
@@ -81,5 +110,13 @@ function createItemCard(item){
         quant.value = String.valueOf(item.item_stock);
     }
 
-   
+    const deleteButtons = document.querySelectorAll('.deleteItem');
+    deleteButtons.forEach(button => {
+        const parentCard = button.parentElement;
+        button.addEventListener('click', () => removeFromCart(parentCard));
+    });
 }
+
+
+
+

baf487e74275896bdc4289e03d603959f844d733
SA <133098645+JKX9@users.noreply.github.com>
2024-03-29 09:02:10 +0300

    P

--- Project/JavaScript/account.js
+++ Project/JavaScript/account.js
@@ -1,16 +1,32 @@
+ import accounts from '../data/accounts.json'
  export default class Account {
 
   static #idCounter = 0;
-  constructor(id, type, firstName, lastName, email, password, purchases) {
+  constructor(id, type, firstName, lastName, username, password, purchases) {
     this.id = id;
+    this.type = type;
     this.firstName = firstName;
     this.lastName = lastName;
-    this.email = email;
+    this.username = username;
     this.password = password;
     this.purchases = purchases;
   }
 
   getID(){
-    
+    return idCounter++;
   }
+
+  static getAccountByUsername(username){
+    const ls = localStorage.getItem('account');
+    if(ls){
+      return JSON.parse(ls);
+    }else{
+      const searchResault = JSON.parse(accounts).find(account => account.username === username);
+      if(searchResault){
+        return searchResault;
+      }
+    }
+    return null;
+  }
+  
 }

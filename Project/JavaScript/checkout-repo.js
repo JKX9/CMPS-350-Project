85f5a504043636242a3b1486f7f9430f36a0a2ba
Essa-ea2105851 <157367635+Essa-ea2105851@users.noreply.github.com>
2024-03-29 07:46:13 +0300

    progress

--- Project/JavaScript/checkout-repo.js
+++ Project/JavaScript/checkout-repo.js
@@ -1,3 +1,41 @@
 import Cart from "./Cart.js";
 import Account from "./account.js";
 import Item from "./Item";
+
+let cartsArray = [];
+let accountsArray = [];
+let purchasesArray = [];
+const account = findAccount();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const userId = account.id;
+    purchasesArray = account.purchases;
+    showCart(3); //userId should be attribute
+});
+
+function showCart(user_id){
+        const cartNeededArr = cartsArray.filter(cart => cart.user_id!=user_id);
+        const cart = cartsArray[0];
+        cart.itemsInCart.forEach(element => {
+            createItemCardCheckout(element)
+        });
+}
+
+const placeOrderBtn = document.getElementById("place-order");
+placeOrderBtn.addEventListener("click", () => {
+    
+});
+
+function createItemCardCheckout(item){
+    const itemCard = document.createElement("div");
+    itemCard.classList.add("item-card");
+    itemCard.dataset.id = item.item_id;
+    itemCard.innerHTML = `
+        <img src=${item.item_img} alt="">
+        <h4>${item.item_name}</h4>
+        <p>Quantity: ${item.quantitySelected}</p>
+        <p>Price:   ${item.item_price}</p>
+    `;
+    const itemHolder = document.getElementById("itemHolder");
+    itemHolder.appendChild(itemCard);
+}

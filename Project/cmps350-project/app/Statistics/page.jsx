071c8e1b5a25aad55fb404b90d88030b2241cbf5
Essa-ea2105851 <157367635+Essa-ea2105851@users.noreply.github.com>
2024-05-09 21:41:53 +0300

    progress

--- Project/cmps350-project/app/Statistics/page.jsx
+++ Project/cmps350-project/app/Statistics/page.jsx
@@ -3,9 +3,19 @@ import React from 'react'
 export default function page() {
   return (
     <>
+      <h1>Statistics</h1>
 
-    
+      <div className="universalCharts">
+            universal charts
+      </div>
 
+      <div className='buyerCharts'>
+            buyer charts
+      </div>
+
+      <div className='sellerCharts'>
+            seller charts
+      </div>
     </>
   )
 }
